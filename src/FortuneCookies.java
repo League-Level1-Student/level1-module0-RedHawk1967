ec0a436ca731c7290a1e98b29d69e45f54b21309
dylanmafong <league@WTS21.attlocal.net>
2019-03-01 18:31:50 -0800

    fortune cookies ip

--- src/FortuneCookies.java
+++ src/FortuneCookies.java
@@ -1,4 +1,59 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FortuneCookies {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class FortuneCookies implements ActionListener {
+public static void main(String[] args) {
+	
+	
+	FortuneCookies fc = new FortuneCookies();
+	fc.showButton();
+
+
+	
+	
+	
+	
+	
+	
+}	
+	public void showButton() {
+	     System.out.println("Button clicked");
+	JFrame frame = new JFrame();
+	JButton button = new JButton();
+	frame.add(button);
+	button.addActionListener(this);
+	frame.setVisible(true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+JOptionPane.showMessageDialog(null,"WooHoo");
+		
+	
+	
+	
+	
+	
+	
+	
+	}}

1127adbf9f092c5ba25512a8542fa7ff81edb0c8
Jack <league@WTS-IM-17.attlocal.net>
2019-03-15 18:27:31 -0700

    fortune cookies done and doing jack in the box need to get ui to activate

--- src/FortuneCookies.java
+++ src/FortuneCookies.java
@@ -1,59 +1,44 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class FortuneCookies implements ActionListener {
-public static void main(String[] args) {
-	
-	
-	FortuneCookies fc = new FortuneCookies();
-	fc.showButton();
+	public static void main(String[] args) {
 
+		FortuneCookies fc = new FortuneCookies();
+		fc.showButton();
 
-	
-	
-	
-	
-	
-	
-}	
+	}
+
+	int rand = new Random().nextInt(5);
+
 	public void showButton() {
-	     System.out.println("Button clicked");
-	JFrame frame = new JFrame();
-	JButton button = new JButton();
-	frame.add(button);
-	button.addActionListener(this);
-	frame.setVisible(true);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+		System.out.println("Button clicked");
+		JFrame frame = new JFrame();
+		JButton button = new JButton("The ultimate fortune cookie");
+		frame.add(button);
+		frame.pack();
+		button.addActionListener(this);
+		frame.setVisible(true);
+	
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-JOptionPane.showMessageDialog(null,"WooHoo");
-		
-	
-	
-	
-	
-	
-	
-	
-	}}
+		int rand = new Random().nextInt(5);
+
+		if (rand == 0) {
+			JOptionPane.showMessageDialog(null, "Some Fortune Cookies Contain No Fortune");
+		} else if (rand == 1) {
+			JOptionPane.showMessageDialog(null, "About Time I got Out Of That Cookie");
+		} else if (rand == 2) {
+			JOptionPane.showMessageDialog(null, "You're Sensitive, Kind, Thoughtful, Wise, and Gullible ");
+		} else if (rand == 3) {
+			JOptionPane.showMessageDialog(null, "Your Pet Is Trying To Eat You");
+		} else if (rand == 4) {JOptionPane.showMessageDialog(null,"The Fortune You Seek Is In Another Cookie");}
+	}
+	}
+
---
+++ src/JackInTheBox.java
@@ -0,0 +1,97 @@
+import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class JackInTheBox implements ActionListener {
+	private void playSound(String soundFile) { 
+	     try {
+	          AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
+	          sound.play();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+
+	private void showPicture(String fileName) { 
+	     try {
+	          JLabel imageLabel = createLabelImage(fileName);
+	          JFrame frame = new JFrame();
+	          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	          frame.add(imageLabel);
+	          frame.setVisible(true);
+	          frame.pack();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+
+	private JLabel createLabelImage(String fileName) {
+	     try {
+	          URL imageURL = getClass().getResource(fileName);
+	          if (imageURL == null) {
+	               System.err.println("Could not find image " + fileName);
+	               return new JLabel();
+	          } else {
+	               Icon icon = new ImageIcon(imageURL);
+	               JLabel imageLabel = new JLabel(icon);
+	               return imageLabel;
+	          }
+	     } catch (Exception e) {
+	          System.err.println("Could not find image " + fileName);
+	          return new JLabel();
+	     }
+	}
+	
+	public static void main(String[] args) {
+System.out.println("hello?");
+	
+	}
+public void ui() {
+
+	JFrame frame = new JFrame();
+	JButton button = new JButton();
+	JPanel panel = new JPanel();
+	panel.add(button);
+	frame.add(panel);
+	frame.setVisible(true);
+	button.addActionListener(this);
+	
+
+
+
+
+
+
+
+
+
+
+
+}
+int count = 0;
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+count++; 
+	if (count == 5) {showPicture("jack.png");
+	
+}	
+
+
+}
+
+
+
+
+
+
+}
